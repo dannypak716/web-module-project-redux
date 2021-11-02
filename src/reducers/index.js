2f7185a58b1c651fc1747f63fc7c482b69dae51b
Jeong Pak <dannypak716@gmail.com>
2021-11-02 15:45:20 -0400

    movies render

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,9 @@ import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
 
+const rootReducer = combineReducers({
+    movieState: movieReducer,
+    // favoritesState: favoritesReducer
+})
+
 export default movieReducer;
